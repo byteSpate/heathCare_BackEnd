78290a97978d4c0808ed2f808edcc381d5ac6a23
Abdullah Al Noman <abdullahalnoman@Abdullahs-Mac-mini.local>
2025-08-08 01:12:34 +0600

    Add modules update working on this module

---
+++ src/app/modules/appointment/appointment.route.ts
@@ -0,0 +1,44 @@
+import express from 'express'
+import { AppointmentController } from './appointment.controller';
+import { authValidation } from '../../middlewares/authValidation';
+import { UserRole } from '../../../generated/prisma';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { AppointmentValidation } from './appointment.validation';
+
+
+const router = express.Router();
+
+/**
+ * ENDPOINT: /appointment/
+ * 
+ * Get all appointment with filtering
+ * Only accessable for Admin & Super Admin
+ */
+router.get(
+    '/',
+    authValidation(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    AppointmentController.getAllFromDB
+);
+
+router.get(
+    '/my-appointment',
+    authValidation(UserRole.PATIENT, UserRole.DOCTOR),
+    AppointmentController.getMyAppointment
+)
+
+router.post(
+    '/',
+    authValidation(UserRole.PATIENT),
+    validateRequest(AppointmentValidation.createAppointment),
+    AppointmentController.createAppointment
+);
+
+router.patch(
+    '/status/:id',
+    authValidation(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+    AppointmentController.changeAppointmentStatus
+);
+
+
+
+export const AppointmentRoutes = router;
